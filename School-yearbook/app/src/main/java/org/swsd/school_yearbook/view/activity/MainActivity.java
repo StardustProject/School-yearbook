d73a1e3854d12f60d1ccc638ef08faea42724d96
Unknown <1009224322@qq.com>
2017-11-04 21:32:12 +0800

    feat(主页): 设置跳转逻辑
    - 设置Menu跳转事件

--- School-yearbook/app/src/main/java/org/swsd/school_yearbook/view/activity/MainActivity.java
+++ School-yearbook/app/src/main/java/org/swsd/school_yearbook/view/activity/MainActivity.java
@@ -1,13 +1,16 @@
 package org.swsd.school_yearbook.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.swsd.school_yearbook.R;
 import org.swsd.school_yearbook.presenter.adapter.NoteAdapter;
@@ -45,12 +48,36 @@ public class MainActivity extends AppCompatActivity{
     private void showPopupMenu(ImageView addImaeView) {
 
         // View当前PopupMenu显示的相对View的位置
-        PopupMenu popupMenu = new PopupMenu(this, addImaeView);
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, addImaeView);
 
         // menu布局
-        popupMenu.getMenuInflater().inflate(R.menu.add_item, popupMenu.getMenu());
-
-
+        getMenuInflater().inflate(R.menu.menu, popupMenu.getMenu());
+        //给菜单绑定监听器
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.add_item:
+                        goAddNewPerson();
+                        break;
+                    case R.id.excel_item:
+                        Toast.makeText(MainActivity.this, "导出excel成功，请在文件管理器中查看", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.photo_item:
+                        Toast.makeText(MainActivity.this, "导出jpg成功，请在文件管理器中查看", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
         popupMenu.show();
+    }
+
+    // 进入到新建同学录界面
+    private void goAddNewPerson(){
+        Intent intent = new Intent(MainActivity.this,NewPersonActivity.class);
+        startActivity(intent);
     }
 }
